98a1792f9c1857947574b21535d507d48da06ecc
infofalles360-lab <infofalles360-lab@users.noreply.github.com>
2026-06-07 20:15:30 +0200

    Add waitlist spam confirmation and customer email flow

--- waitlist-config.js
+++ waitlist-config.js
@@ -1,6 +1,6 @@
 window.FALLES360_WAITLIST = Object.assign(
   {
-    endpoint: "https://formsubmit.co/ajax/info.falles360@gmail.com",
+    endpoint: "https://formsubmit.co/info.falles360@gmail.com",
     countEndpoint: "",
     initialCount: 0,
     payloadFormat: "json",
@@ -9,7 +9,7 @@ window.FALLES360_WAITLIST = Object.assign(
     landingHref: "./index.html",
     successTitle: "Ya estas dentro.",
     successMessage: "Te avisaremos antes que nadie cuando abramos el acceso anticipado.",
-    successHref: "https://infofalles360-lab.github.io/fallas360-whitelist/?enviado=1",
+    successHref: "https://infofalles360-lab.github.io/fallas360-whitelist/?enviado=1&popup=1",
     formHref: "https://infofalles360-lab.github.io/fallas360-whitelist/",
   },
   window.FALLES360_WAITLIST || {},
